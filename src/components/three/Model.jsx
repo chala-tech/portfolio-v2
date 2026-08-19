196385b0920599797f62071f97f7759d52dc6161
Chala Fokora <chala.ugr-0653-17@aau.edu.et>
2026-08-19 07:56:24 +0300

    feat: add mouse-parallax tilt to Hero 3D wireframe

--- src/components/three/Model.jsx
+++ src/components/three/Model.jsx
@@ -1,13 +1,20 @@
 import { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 
 
 export default function Model() {
   const meshRef = useRef();
+  const { pointer } = useThree();
 
   useFrame((state, delta) => {
-    meshRef.current.rotation.x += delta * 0.15;
-    meshRef.current.rotation.y += delta * 0.2;
+    
+    meshRef.current.rotation.y += delta * 0.15;
+
+    
+    const targetX = pointer.y * 0.4;
+    const targetZ = pointer.x * -0.4;
+    meshRef.current.rotation.x += (targetX - meshRef.current.rotation.x) * 0.05;
+    meshRef.current.rotation.z += (targetZ - meshRef.current.rotation.z) * 0.05;
   });
 
   return (
--- src/components/three/Scene.jsx
+++ src/components/three/Scene.jsx
@@ -4,7 +4,7 @@ import Model from "./Model";
 
 export default function Scene() {
   return (
-    <Canvas camera={{ position: [0, 0, 5], fov: 45 }}>
+    <Canvas camera={{ position: [0, 0, 5], fov: 45 }} eventSource={document.body}>
       <ambientLight intensity={0.5} />
       <Model />
     </Canvas>
